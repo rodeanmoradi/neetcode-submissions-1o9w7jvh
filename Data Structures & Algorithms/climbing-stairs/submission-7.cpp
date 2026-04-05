b4f7d8bf5e41099f367c002dffa3655b45afcc8e
Rodean Moradi <rodean.moradi@gmail.com>
2026-04-05 15:25:36 -0400

    Add: climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/climbing-stairs/submission-7.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    // Smart recursion/bottom-up dynamic programming approach
+    std::unordered_map<int, int> paths;
+    int climbStairs(int n) {
+        if(n < 2) {
+            return 1;
+        }   
+        
+        if(n == 2) {
+            return 2;
+        } 
+
+        if(!paths.contains(n)) {
+            paths[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        }
+        return paths[n];
+    }
+};
