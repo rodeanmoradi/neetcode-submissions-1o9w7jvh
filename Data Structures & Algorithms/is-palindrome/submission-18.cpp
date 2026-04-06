07ca32e8f20503ce961b8e5a4bb5c619c5f915e9
Rodean Moradi <rodean.moradi@gmail.com>
2026-04-06 18:49:17 -0400

    Add: is-palindrome - submission-18

---
+++ Data Structures & Algorithms/is-palindrome/submission-18.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        // O(n) time, O(n) space; optimal is O(1) space because new_s vector not needed
+        vector<char> new_s;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] >= 48 && s[i] <= 57) {
+                new_s.push_back(s[i]);
+            }
+            else if(s[i] >= 65 && s[i] <= 90) {
+                new_s.push_back(s[i]);
+            }
+            else if(s[i] >= 97 && s[i] <= 122) {
+                new_s.push_back(s[i]);
+            }
+        }
+
+        if(new_s.size() == 1 || new_s.size() == 0) {
+            return true;
+        }
+
+        char* left = &new_s[0];
+        char* right = left + new_s.size() - 1;
+        for(int i = 0; i < new_s.size(); i++) {
+            if(left == right || left - right == 1) {
+                return true;
+            }
+            if(*left == *right) {
+                left += 1;
+                right -= 1;
+            }
+            // Check if lowercase and uppercase pair
+            // First check if > 57 so the check does not run for numbers
+            else if(*left > 57 && *left + 32 == *right) {
+                left += 1;
+                right -= 1;
+            }
+            else if(*right > 57 && *left - 32 == *right) {
+                left += 1;
+                right -= 1;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+};
