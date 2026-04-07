fdee89444979e6364abdefe276762c942578764f
Rodean Moradi <rodean.moradi@gmail.com>
2026-04-07 12:33:19 -0400

    Add: binary-search - submission-18

---
+++ Data Structures & Algorithms/binary-search/submission-18.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) { 
+        int start = 0;
+        int end = nums.size() - 1;
+        while(end >= start) {
+            if(nums[(start + end) / 2] == target) {
+                return (start + end) / 2;
+            }
+            else if(target > nums[(start + end) / 2]) {
+                start = (start + end) / 2 + 1;
+            }
+            else {
+                end = (start + end) / 2 - 1;
+            }
+            
+        }
+
+        return -1;
+    }
+};
