5da9e646ad4f6bc1f83681c4e4a6c7f9ad56c6c6
Rodean Moradi <rodean.moradi@gmail.com>
2026-04-10 14:34:26 -0400

    Add: longest-substring-without-duplicates - submission-128

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-128.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> hash;
+        int best_len = 0;
+        int start = 0;
+        for(int i = 0; i < s.size(); i++) {
+            if(!hash.contains(s[i])) {
+                hash[s[i]] = i;
+            }
+            else {
+                if(hash[s[i]] >= start) {
+                    start = hash[s[i]] + 1;
+                    hash[s[i]] = i;
+                }
+                else {
+                    hash[s[i]] = i;
+                }
+            }
+            if((i - start + 1) > best_len) {
+                best_len = i - start + 1;
+            }
+        } 
+        return best_len;
+    }
+};
