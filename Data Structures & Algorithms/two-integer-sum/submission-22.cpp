08d69a795071043136a72540706adad8bf793358
Rodean Moradi <rodean.moradi@gmail.com>
2026-04-12 13:24:17 -0400

    Add: two-integer-sum - submission-22

---
+++ Data Structures & Algorithms/two-integer-sum/submission-22.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       // loop through array
+       // create hash map as you go
+       // diff = target - nums[i]
+       // search for diff, ensuring its not at the same index 
+        std::unordered_map<int, int> map;
+        for(int i = 0; i < nums.size(); i++) {
+            map[nums[i]] = i;
+        }
+        for(int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+            if(map.count(diff) == 0) {
+                continue;
+            }
+            int j = map[diff];
+            if(i != j) {
+                vector<int> ans;
+                ans.push_back(i);
+                ans.push_back(j);
+
+                return ans;
+            }
+        }
+    }
+};
