ef4890f9e61ba12df8746bdd3ebe3a52a4ab2337
Rodean Moradi <rodean.moradi@gmail.com>
2026-09-05 23:12:23 -0400

    Add: duplicate-integer - submission-46

---
+++ Data Structures & Algorithms/duplicate-integer/submission-46.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        for(int i = 0; i + 1 < nums.size(); i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
