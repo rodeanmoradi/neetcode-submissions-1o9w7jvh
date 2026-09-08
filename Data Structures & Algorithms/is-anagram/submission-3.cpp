ae619da77aede352e589cdbd5945c92e30f1addf
Rodean Moradi <rodean.moradi@gmail.com>
2026-09-07 22:44:43 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       std::sort(s.begin(), s.end());
+       std::sort(t.begin(), t.end());
+       if (s == t) {
+          return true;
+       }
+       else {
+          return false;
+       }
+    }
+};
