45bc9cfe42998678ebdec995f412ac239ddb67ef
Rodean Moradi <rodean.moradi@gmail.com>
2026-09-09 00:35:02 -0400

    Add: two-integer-sum - submission-32

---
+++ Data Structures & Algorithms/two-integer-sum/submission-32.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> nums_map;
+
+        for(int i = 0; i < nums.size(); i++) {
+            nums_map[nums[i]] = i;
+        }
+
+        for(int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+            if (nums_map.contains(diff)) {
+                int j = nums_map[diff];
+                if (i != j) {
+                    vector<int> indices = {i, j};
+                    std::sort(indices.begin(), indices.end());
+                    return indices;
+                }
+            }
+        }
+    }
+};
